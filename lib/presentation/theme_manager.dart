dfd470b5b2afd9957749e31ba496c2baa5ed789a
Thae00 <mathaeoo97@gmail.com>
2022-07-15 12:38:15 +0630

    creating appBar Theme  manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:adv_tut/presentation/color_manager.dart';
+import 'package:adv_tut/presentation/font_manager.dart';
+import 'package:adv_tut/presentation/styles_manager.dart';
 import 'package:adv_tut/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,8 @@ ThemeData getApplicationTheme() {
     primaryColorDark: ColorManager.darkPrimary,
 
     disabledColor: ColorManager.grey1, // will be used incase of disabled btn for example
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
     //accentColor: ColorManager.grey, // accentColor is deprecated and shouldn't be used
     colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey), // instead accentColor
 
@@ -19,7 +23,18 @@ ThemeData getApplicationTheme() {
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
     ),
+
     // App bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white, 
+        fontSize: FontSize.s16,
+      ) ,
+    ),
 
     // Button theme
 
