568fdc176d36a393aed6771591bd6d884fdba22e
Thae00 <mathaeoo97@gmail.com>
2022-07-26 15:55:08 +0630

    start splash screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:adv_tut/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 import '../presentation/resources/theme_manager.dart';
 
@@ -21,6 +22,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
     );
   }
